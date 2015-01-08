3a5791e04dee3637ace595d53ab1923ac4c27abd
dboyliao <qmalliao@gmail.com>
2015-01-08 16:45:33 +0800

    Logging version2. Enable capibility of export logging without locking down terminal shell.

--- lib/config_sample.py
+++ lib/config_sample.py
@@ -2,6 +2,7 @@ MONGO_ADMIN = "admin" # Username for user administrator.
 MONGO_ADMIN_PWD = "adminpwd" # Password for user administrator.
 MONGO_PORT = 27017 # Port for mongod to run at.
 MONGO_DBPATH = '/data/db' # Data files path for mongod.
+MONGO_LOGPATH = '/share/mongodb.log'
 # Users to be added.
 MONGO_USERS = [
 	{"user":"mongo", "pwd":"mongo", "roles":[{"role":"readWrite", "db":"test"}]},
